1db716df719b459df778ee61682481f57e3fbbfb
xiaminghong <1562911656@qq.com>
2018-12-09 22:46:09 +0800

    首页侧边菜单动态渲染

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -11,7 +11,7 @@
   <el-col :span="7">
     <div class="out">
       <span>欢迎上海前端28期星曜会员</span>
-       <el-button type="text" @click="open7">
+       <el-button type="text" @click="logout">
          <a href="javascript:;">退出</a>
        </el-button>
          <!-- <el-button type="text" @click="open6"><a href="javascript:;">退出</a></el-button> -->
@@ -24,78 +24,25 @@
       <!-- 左侧导航 -->
       <el-menu
         :router='true'
-        default-active="2"
+        :unique-opened='true'
+        :default-active="$route.path.slice(1)"
         class="el-menu-vertical-demo"
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b">
         <!-- 用户管理 -->
-        <el-submenu index="1">
+        <el-submenu :index="level1.id + ''" v-for="level1 in menus" :key="level1.id">
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span>用户管理</span>
+            <span>{{level1.authName}}</span>
           </template>
-          <el-menu-item index="/users">
+          <!-- 二级菜单 -->
+          <el-menu-item :index="level2.path" v-for='level2 in level1.children' :key="level2.id">
             <i class="el-icon-menu"></i>
-            <span slot="title">用户列表</span>
+            <span slot="title">{{level2.authName}}</span>
           </el-menu-item>
         </el-submenu>
         <!-- 权限管理 -->
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>权限管理</span>
-          </template>
-          <el-menu-item index="/roles">
-            <i class="el-icon-menu"></i>
-            <span slot="title">角色列表</span>
-          </el-menu-item>
-          <el-menu-item index="/rights">
-            <i class="el-icon-menu"></i>
-            <span slot="title">权限列表</span>
-          </el-menu-item>
-        </el-submenu>
-        <!-- 商品管理 -->
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>商品管理</span>
-          </template>
-          <el-menu-item index="/goods">
-            <i class="el-icon-menu"></i>
-            <span slot="title">商品列表</span>
-          </el-menu-item>
-          <el-menu-item index="/params">
-            <i class="el-icon-menu"></i>
-            <span slot="title"> 分类参数</span>
-          </el-menu-item>
-          <el-menu-item index="/categories">
-            <i class="el-icon-menu"></i>
-            <span slot="title">商品分类</span>
-          </el-menu-item>
-        </el-submenu>
-        <!-- 订单管理 -->
-        <el-submenu index="4">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>订单管理</span>
-          </template>
-          <el-menu-item index="1-7">
-            <i class="el-icon-menu"></i>
-            <span slot="title">订单列表</span>
-          </el-menu-item>
-        </el-submenu>
-        <!-- 数据统计 -->
-        <el-submenu index="5">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>数据统计</span>
-          </template>
-          <el-menu-item index="1-8">
-            <i class="el-icon-menu"></i>
-            <span slot="title">数据报表</span>
-          </el-menu-item>
-        </el-submenu>
       </el-menu>
     </el-aside>
 
@@ -108,9 +55,16 @@
 
 <script>
 export default {
-
+  created () {
+    this.getMenus()
+  },
+  data () {
+    return {
+      menus: []
+    }
+  },
   methods: {
-    open7 () {
+    logout () {
       this.$confirm('您确定退出吗?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -131,6 +85,12 @@ export default {
             message: '已取消退出'
           })
         })
+    },
+    async getMenus () {
+      const res = await this.$http.get('menus')
+      console.log(res)
+      this.menus = res.data.data
+      console.log(this.menus)
     }
   }
 }
